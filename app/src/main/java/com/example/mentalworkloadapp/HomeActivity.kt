f1b0b95ae82a08e62a95fe72ecfcc78039916557
Lorenzo Valtriani <lorenzovaltriani@gmail.com>
2025-05-10 23:19:02 +0200

    Layout Login, Signup
    Create a responsive layout for login and signup. And I also create a logo and a name for our App: "Selene".

---
+++ app/src/main/java/com/example/mentalworkloadapp/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mentalworkloadapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+    }
+}
--- app/src/main/java/com/example/mentalworkloadapp/SignupActivity.kt
+++ app/src/main/java/com/example/mentalworkloadapp/SignupActivity.kt
@@ -1,11 +1,31 @@
 package com.example.mentalworkloadapp
 
+import android.R.attr.logo
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class SignupActivity : ComponentActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signup)
+
+        // To hidden the logo if the user switch to landscape
+        val logo = findViewById<ImageView>(R.id.appLogo)
+        if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) logo.visibility = View.GONE
+        else logo.visibility = View.VISIBLE
+
+        // If you are already registered, you can login in the login activity
+        val registrationLink = findViewById<TextView>(R.id.areYouAlreadyRegistered)
+        registrationLink.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
